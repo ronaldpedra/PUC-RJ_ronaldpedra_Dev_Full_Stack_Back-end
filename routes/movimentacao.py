fca075d67e4b0564356e9d87d9743bdb7f1dd478
Ronald Pedra <ronaldpedra@gmail.com>
2025-11-22 04:16:24 -0300

    Update movimentacao.py

--- routes/movimentacao.py
+++ routes/movimentacao.py
@@ -21,11 +21,14 @@ api = APIBlueprint('movimentacao', __name__, url_prefix='/movimentacoes',
 
 @api.post('/',
           summary='Adiciona uma nova Movimentação',
-          description='Adiciona uma Movimentação à base de dados e retorna uma representação da mesma.',
-          responses={'200': schemas.MovimentacaoViewSchema, '409': schemas.ErrorSchema,
+          description='Adiciona uma Movimentação à base de dados e ' \
+          'retorna uma representação da mesma.',
+          responses={'200': schemas.MovimentacaoViewSchema, \
+                     '409': schemas.ErrorSchema,
                      '400': schemas.ErrorSchema})
 def add_movimentacao(form: schemas.MovimentacaoPostSchema):
-    """Adiciona uma Movimentação à base de dados e retorna uma representação da Movimentação"""
+    """Adiciona uma Movimentação à base de dados e retorna uma 
+    representação da Movimentação"""
     movimentacao = Movimentacao(
         movimento = form.movimento,
         ticker = form.ticker,
@@ -35,8 +38,8 @@ def add_movimentacao(form: schemas.MovimentacaoPostSchema):
         preco_medio = form.preco_medio,
         total_operacao = form.total_operacao,
         total_investido = form.total_investido,
-        lucro_operacao = form.lucro_operacao, # TODO: calcular
-        lucro_investimento = form.lucro_investimento # TODO: calcular
+        lucro_operacao = form.lucro_operacao,
+        lucro_investimento = form.lucro_investimento
     )
     try:
         session = Session()
@@ -52,8 +55,10 @@ def add_movimentacao(form: schemas.MovimentacaoPostSchema):
 
 @api.get('/carteira',
          summary='Retorna a carteira de investimentos atual',
-         description='Retorna o último registro de cada ativo que já foi negociado, mesmo que a quantidade atual seja zero.',
-         responses={'200': schemas.ListarCarteiraSchema, '404': schemas.ErrorSchema})
+         description='Retorna o último registro de cada ativo que já foi ' \
+         'negociado, mesmo que a quantidade atual seja zero.',
+         responses={'200': schemas.ListarCarteiraSchema, \
+                    '404': schemas.ErrorSchema})
 def get_carteira():
     """Retorna a carteira de investimentos do usuário.
 
